5a557af7d4bc6a272827ffcac99af6c1ce52a411
RitickLath <ritick943lath@gmail.com>
2024-08-23 17:30:15 +0530

    Structure page

--- app/(pages)/(Owner)/structure/page.jsx
+++ app/(pages)/(Owner)/structure/page.jsx
@@ -23,12 +23,27 @@ const places = [
 
 const Structure = () => {
   return (
-    <div className="px-6 mx-auto pt-10 md:h-[80vh] lg:px-32 flex flex-col justify-center">
-      <h1 className="text-2xl">Which of these best describes your place?</h1>
+    <div className="mx-auto pt-10 px-6 flex flex-col justify-center items-center">
+      <h1 className="text-2xl text-center">
+        Which of these best describes your place?
+      </h1>
 
       {/*  */}
+      <div className="mt-12 grid grid-cols-2 md:grid-cols-3 gap-3">
+        {places.map((title, i) => (
+          <Box key={i} title={title} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Structure;
+
+const Box = ({ title }) => {
+  return (
+    <div className="w-[150px] h-[75px] sm:w-[200px] sm:h-[100px] border-2 border-gray-400 flex justify-start items-center px-4 rounded-md hover:border-black">
+      <h1>{title}</h1>
+    </div>
+  );
+};
